435462278fca447a08cfbed2eabb561ac7f88500
Cihan Sari <cihansari86@gmail.com>
2021-11-07 12:11:08 +0100

    Allow multiple parameters

--- observer/include/csari/observer.hpp
+++ observer/include/csari/observer.hpp
@@ -13,6 +13,25 @@ namespace csari {
 using Subscription = std::shared_ptr<void>;
 namespace ob_internal {
 
+template <typename... Args>
+struct ObserverArgumentHelper {
+  using IsTuple = std::true_type;
+  using Container = std::tuple<Args...>;
+  using F = std::function<void(Args...)>;
+};
+template <typename T>
+struct ObserverArgumentHelper<T> {
+  using IsTuple = std::false_type;
+  using Container = T;
+  using F = std::function<void(T)>;
+};
+template <>
+struct ObserverArgumentHelper<void> {
+  using IsTuple = std::false_type;
+  using Container = void;
+  using F = std::function<void()>;
+};
+
 template <typename T>
 struct ObserverMemoryType {
   // Memory keeps list of content that should be sent to each observer
@@ -25,15 +44,6 @@ struct ObserverMemoryType<void> {
   using MemoryType = std::size_t;
 };
 
-template <typename T>
-struct Function {
-  using F = std::function<void(T)>;
-};
-template <>
-struct Function<void> {
-  using F = std::function<void()>;
-};
-
 // Generate a "unique" id per subscription to self-clean.
 auto getNextId() {
   static std::atomic_size_t sNextId{};
@@ -42,14 +52,16 @@ auto getNextId() {
 
 // Core observer. All callbacks, subscriptions and memory is shared with this
 // object.
-template <typename T, class F>
+template <typename ArgumentHelper>
 struct ObserverCore final {
+  using F = typename ArgumentHelper::F;
   // Keeps track of subscriber ids and their callback functions
   std::unordered_map<std::size_t, F> m_map;
   // thread safety
   std::mutex m_mutex;
   // contains std::deque<T> if non-void, std::size_t otherwise
-  typename ObserverMemoryType<T>::MemoryType m_memory{};
+  typename ObserverMemoryType<typename ArgumentHelper::Container>::MemoryType
+      m_memory{};
   // maximum memory size
   std::size_t m_nMemory{0u};
 
@@ -57,7 +69,7 @@ struct ObserverCore final {
   void setMemorySize(std::size_t const nMemory) {
     auto const lock = std::lock_guard{m_mutex};
     m_nMemory = nMemory;
-    if constexpr (std::is_same_v<T, void>) {
+    if constexpr (std::is_same_v<ArgumentHelper::Container, void>) {
       if (m_memory > m_nMemory) {
         m_memory = m_nMemory;
       }
@@ -74,12 +86,19 @@ struct ObserverCore final {
   }
 
   void callbackFromMemory(F &callback) {
-    if constexpr (std::is_same_v<T, void>) {
+    if constexpr (std::is_same_v<ArgumentHelper::Container, void>) {
       for (auto i = std::size_t{0}; i < m_memory; ++i) {
         callback();
       }
     } else {
-      std::for_each(cbegin(m_memory), cend(m_memory), callback);
+      std::for_each(cbegin(m_memory), cend(m_memory),
+                    [&callback](ArgumentHelper::Container const &args) {
+                      if constexpr (ArgumentHelper::IsTuple::value) {
+                        std::apply(callback, args);
+                      } else {
+                        std::invoke(callback, args);
+                      }
+                    });
     }
   }
 
@@ -95,8 +114,6 @@ struct ObserverCore final {
 
   template <typename... Args>
   auto appendMemory(Args &&...value) {
-    static_assert(sizeof...(Args) < 2,
-                  "appendMemory accepts maximum one parameter");
     if (m_nMemory == 0) {
       return;
     }
@@ -105,7 +122,7 @@ struct ObserverCore final {
         m_memory.pop_front();
       }
       m_memory.emplace_back(std::forward<Args>(value)...);
-    } else if constexpr (std::is_same_v<T, void>) {
+    } else if constexpr (std::is_same_v<ArgumentHelper, void>) {
       if (m_memory < m_nMemory) {
         ++m_memory;
       }
@@ -129,10 +146,10 @@ struct ObserverCore final {
 // Acts as a scope-guard. It will unsubscribe when the object is discarded
 // (out of scope) or manually released. Unique per subscription. Shared to the
 // caller with shared_ptr to manage the lifetime of the subscription.
-template <typename T, class F>
+template <typename ArgumentHelper>
 class SubscriptionBase final {
  public:
-  using WeakO = std::weak_ptr<ObserverCore<T, F>>;
+  using WeakO = std::weak_ptr<ObserverCore<ArgumentHelper>>;
   SubscriptionBase(WeakO &&d, std::size_t const idx)
       : m_weakD{std::forward<WeakO>(d)}, m_idx{idx} {}
   ~SubscriptionBase() {
@@ -156,9 +173,10 @@ class SubscriptionBase final {
 };
 
 // Subscription helper
-template <typename T, class F>
-[[nodiscard]] auto subscribe(std::shared_ptr<ObserverCore<T, F>> &&d,
-                             F callback) -> Subscription {
+template <typename ArgumentHelper>
+[[nodiscard]] auto subscribe(std::shared_ptr<ObserverCore<ArgumentHelper>> &&d,
+                             typename ObserverCore<ArgumentHelper>::F callback)
+    -> Subscription {
   // Access shared elements via lock-guard
   auto const idxSubscription = [&] {
     auto const lock = std::lock_guard{d->m_mutex};
@@ -167,13 +185,13 @@ template <typename T, class F>
   // Perform cached callbacks without any lock
   d->callbackFromMemory(callback);
   // Return subscription object
-  return std::make_shared<SubscriptionBase<T, F>>(std::move(d),
-                                                  idxSubscription);
+  return std::make_shared<SubscriptionBase<ArgumentHelper>>(std::move(d),
+                                                            idxSubscription);
 }
 
-template <typename T, class F>
+template <typename ArgumentHelper>
 class ObservableBase final {
-  using ObserverCore = ob_internal::ObserverCore<T, F>;
+  using ObserverCore = ob_internal::ObserverCore<ArgumentHelper>;
   std::weak_ptr<ObserverCore> d;
 
  public:
@@ -187,9 +205,11 @@ class ObservableBase final {
   bool isAlive() const { return !d.expired(); }
 
   // Subscribe to the core if it still exists, returns nullopt otherwise.
-  [[nodiscard]] auto subscribe(F &&callback) -> Subscription {
+  [[nodiscard]] auto subscribe(typename ObserverCore::F &&callback)
+      -> Subscription {
     if (auto s_d = d.lock()) {
-      return ob_internal::subscribe(std::move(s_d), std::forward<F>(callback));
+      return ob_internal::subscribe(
+          std::move(s_d), std::forward<typename ObserverCore::F>(callback));
     } else {
       return nullptr;
     }
@@ -199,9 +219,9 @@ class ObservableBase final {
   auto share() const { return ObservableBase{std::weak_ptr<ObserverCore>{d}}; }
 };
 
-template <typename T, class F>
+template <typename ArgumentHelper>
 class SubjectBase final {
-  using ObserverCore = ob_internal::ObserverCore<T, F>;
+  using ObserverCore = ob_internal::ObserverCore<ArgumentHelper>;
 
   // Create a core
   std::shared_ptr<ObserverCore> d = std::make_shared<ObserverCore>();
@@ -214,17 +234,19 @@ class SubjectBase final {
   SubjectBase() = default;
 
   // Store the returned subscription to receive further callbacks
-  [[nodiscard]] auto subscribe(F &&callback) -> Subscription {
-    return ob_internal::subscribe(std::shared_ptr<ObserverCore>{d},
-                                  std::forward<F>(callback));
+  [[nodiscard]] auto subscribe(typename ObserverCore::F &&callback)
+      -> Subscription {
+    return ob_internal::subscribe(
+        std::shared_ptr<ObserverCore>{d},
+        std::forward<typename ObserverCore::F>(callback));
   }
 
   // Number of triggers stored for new subscribers
   void setMemorySize(std::size_t const nMemory) { d->setMemorySize(nMemory); }
 
   // Create a sharable shallow observable. Points to the same core.
-  [[nodiscard]] auto asObservable() const -> ObservableBase<T, F> {
-    return ObservableBase<T, F>{d};
+  [[nodiscard]] auto asObservable() const -> ObservableBase<ArgumentHelper> {
+    return ObservableBase<ArgumentHelper>{d};
   }
 
   // Create a sharable shallow subject. Both subjects point to the same core.
@@ -234,26 +256,23 @@ class SubjectBase final {
 
   template <typename... Args>
   auto operator<<(Args &&...value) -> SubjectBase & {
-    static_assert(sizeof...(Args) < 2,
-                  "pipe operator accepts maximum one parameter");
     d->next(std::forward<Args>(value)...);
     return *this;
   }
 
   template <typename... Args>
   auto next(Args &&...value) -> SubjectBase & {
-    static_assert(sizeof...(Args) < 2, "next accepts maximum one parameter");
     d->next(std::forward<Args>(value)...);
     return *this;
   }
 };
 }  // namespace ob_internal
 
-template <typename T>
-using Subject =
-    ob_internal::SubjectBase<T, typename ob_internal::Function<T>::F>;
+template <typename... Args>
+using Subject = ob_internal::SubjectBase<
+    typename ob_internal::ObserverArgumentHelper<Args...>>;
 
-template <typename T>
-using Observable =
-    ob_internal::ObservableBase<T, typename ob_internal::Function<T>::F>;
+template <typename... Args>
+using Observable = ob_internal::ObservableBase<
+    typename ob_internal::ObserverArgumentHelper<Args...>>;
 }  // namespace csari
